24352fe5c47eb2c10f9e88394ef023b754e23da6
Thiago <thiago.trivelato@maxipago.com>
2017-02-03 08:31:13 -0200

    Alteracao valores maisculo

--- 1.6.x-1.9.x/app/code/community/MaxiPago/CheckoutApi/Model/Source/Saleschannel.php
+++ 1.6.x-1.9.x/app/code/community/MaxiPago/CheckoutApi/Model/Source/Saleschannel.php
@@ -4,10 +4,11 @@ class MaxiPago_CheckoutApi_Model_Source_Saleschannel
 {
 	public function toOptionArray()
 	{
+                //Necessario enviar Maisculo
 		return array(
-				array('value' => 'Web', 'label' => Mage::helper('adminhtml')->__('E-commerce')),
-				array('value' => 'Phone', 'label' => Mage::helper('adminhtml')->__('Call-center')),
-				array('value' => 'Outros', 'label' => Mage::helper('adminhtml')->__('Outros')),
+				array('value' => 'DEFAULT', 'label' => Mage::helper('adminhtml')->__('E-commerce')),
+				array('value' => 'PHONE', 'label' => Mage::helper('adminhtml')->__('Call-center')),
+				array('value' => 'OUTROS', 'label' => Mage::helper('adminhtml')->__('Outros')),
 		);
 	}
 }
